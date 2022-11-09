fd5a4db74ba72eb3f5f79dadb522975ef57b9c59
Vladyslav Matsapura <84621365+vladelleus@users.noreply.github.com>
2022-11-09 09:27:00 +0200

    Update web-server.sh

--- web-server.sh
+++ web-server.sh
@@ -4,3 +4,4 @@ sudo apt-get -y install apache2
 sudo chown -R $USER:$USER /var/www/
 sudo chmod 755 /var/www/
 echo "<html><body><h1>Vladyslav Matsapura</h1></body></html>" > /var/www/html/index.html
+sudo service apache2 start
